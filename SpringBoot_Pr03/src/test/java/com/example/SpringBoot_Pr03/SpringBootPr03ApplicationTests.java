23663b46c3ed2b3108d435cee36fbf74ed3e2626
SergioArenas98 <sejuma18@gmail.com>
2024-10-01 16:31:16 +0200

    first commit

---
+++ SpringBoot_Pr03/src/test/java/com/example/SpringBoot_Pr03/SpringBootPr03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_Pr03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPr03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
