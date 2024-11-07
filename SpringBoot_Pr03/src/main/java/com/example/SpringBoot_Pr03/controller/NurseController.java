ed29364678a4ba793771c9a1cbd5907cbb983446
SergioArenas98 <sejuma18@gmail.com>
2024-11-07 17:11:36 +0100

    RESTful URI should now refers to a resource that is a thing (noun) #47

--- SpringBoot_Pr03/src/main/java/com/example/SpringBoot_Pr03/controller/NurseController.java
+++ SpringBoot_Pr03/src/main/java/com/example/SpringBoot_Pr03/controller/NurseController.java
@@ -69,7 +69,7 @@ public class NurseController {
     	}
     }
     
-    @PutMapping("/update/{nurseId}")
+    @PutMapping("/{nurseId}")
     public ResponseEntity<Nurse> update(@PathVariable int nurseId, @RequestBody Nurse nurse) {
     	
         // Check if Nurse object is null
